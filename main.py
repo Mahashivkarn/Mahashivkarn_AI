2ce53b892e5a41bbe9d909f20e55bbed22e5eaec
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-05 15:48:26 +0530

    Name MahaShivKarnAI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/create")
+@app.route("/create", methods=["GET","POST"])
 def create():
     return render_template("create.html")
 
